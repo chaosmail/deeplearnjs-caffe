23e591b50027b52d1d88ab89a0738b6d51cb8c81
Christoph Koerner <office@chaosmail.at>
2018-01-21 17:59:50 +0100

    fix karma command

--- test/e2e/run.sh
+++ test/e2e/run.sh
@@ -5,4 +5,4 @@ sh "$(dirname $0)/squeezenet/run.sh"
 sh "$(dirname $0)/googlenet/run.sh"
 
 # Run Karma test runner
-node_modules/.bin/karma start $@ karma.e2e.conf.js
+node_modules/.bin/karma start karma.e2e.conf.js $@
